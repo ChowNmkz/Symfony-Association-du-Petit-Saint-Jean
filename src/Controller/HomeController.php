fef75f0cd12479dfbf1dec1445ddc8ec17af2145
Gay Kévin <gay.kevin528@gmail.com>
2021-12-06 21:32:48 +0100

    Update : Page d'arrivée
    - Modification de la page d'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\CommerceQuartier;
+use App\Entity\Doleances;
+use App\Entity\Evenement;
+use App\Entity\ImportPdf;
+use App\Entity\ImportPhoto;
+use App\Entity\Report;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +17,45 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'Acceuil',
-        ]);
+        $commerce = $this->getDoctrine()
+        ->getRepository(CommerceQuartier::class)
+        ->findAll();
+    //dd($commerce);
+    
+    $doleance = $this->getDoctrine()
+        ->getRepository(Doleances::class)
+        ->findAll();
+    //dd($doleance);
+
+    $evenement = $this->getDoctrine()
+        ->getRepository(Evenement::class)
+        ->findAll();
+    //dd($evenement);
+
+    $rapport = $this->getDoctrine()
+        ->getRepository(Report::class)
+        ->findAll();
+    //dd($rapport);
+
+    $gazette = $this->getDoctrine()
+        ->getRepository(ImportPdf::class)
+        ->findAll();
+    //dd($gazette);
+
+    $photo = $this->getDoctrine()
+        ->getRepository(ImportPhoto::class)
+        ->findAll();
+    //dd($photo);
+
+    return $this->render('home/index.html.twig', [
+        'evenement' => $evenement,
+        'commerce' => $commerce,
+        'doleance' => $doleance,
+        'rapport' => $rapport,
+        'gazette' => $gazette,
+        'photo' => $photo
+    ]);
     }
 }
+
+
